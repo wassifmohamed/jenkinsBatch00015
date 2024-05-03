d9dc4c7ae12fa5edf5bda2dfdf04e24041b99f77
wassi <wassifmohamed@gmail.com>
2024-05-03 09:55:03 -0400

    Array

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,25 @@ public class Main {
             Thread.sleep(1000);
 
 
+            System.out.println(" Array first class");
+
+            int [] arr={10,20,30,40,50};
+            System.out.println(arr[4]);
+            // Can we print two numbes from an array
+            System.out.println(arr[1]+" "+arr[4]);
+            // print all the elements from this array using a loop
+
+            for (int j = 0; j < arr.length; j++) {
+                System.out.println(arr[i]);
+            }
+            System.out.println("*************");
+            // Enhanced/foreach for loop
+            // when ever possible use below loop
+            for(int number:arr){
+                System.out.println(number);
+            }
+
+
         }
     }
 }
